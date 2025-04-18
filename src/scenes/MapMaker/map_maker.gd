527340328426ac648a2a50399cc0787b33a81e52
Wayne <crazyfredsteve@gmail.com>
2025-04-18 14:03:20 -0400

    Renamed map maker base node, created map variable, new debug button
    Added a map variable to begin tracking the map. Currently it updates with every area added when pressing the NewArea button.
    New debug button prints the current map.

--- src/scenes/MapMaker/map_maker.gd
+++ src/scenes/MapMaker/map_maker.gd
@@ -1,3 +1,13 @@
 extends Control
 
 @onready var bgloader = %BGLoader
+
+@onready var map: Array = []
+
+func _on_new_area_area_passer(area: Variant) -> void:
+	map.append(area)
+	pass # Replace with function body.
+
+
+func _on_print_map_pressed() -> void:
+	pass # Replace with function body.
--- src/scenes/MapMaker/new_area.gd
+++ src/scenes/MapMaker/new_area.gd
@@ -1,11 +1,12 @@
 extends Button
-
+signal area_passer(area)
 var new_area_scene = preload("uid://xseu36qbixsb")
 
 func _on_pressed() -> void:
 	var new_area = new_area_scene.instantiate()
 	%AreaContainer.add_child(new_area)
 	new_area.changebg.load_bg.connect(change_bg)
+	emit_signal("area_passer", new_area)
 	pass # Replace with function body.
 
 func change_bg():
