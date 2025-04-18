027b4e512a26c2ca1500741badb944c7f38a3ce8
Wayne <crazyfredsteve@gmail.com>
2025-04-18 12:18:52 -0400

    Linked AreaNameInput to base node so that area name is properly updated
    Also added a temporary print statement that updates every time the box's text is updated.
    Also also, added a variable to area_maker.gd to hold the link to the bg img. Put a key in the dictionary for this as well.

--- src/scenes/MapMaker/AreaDefiner/area_maker.gd
+++ src/scenes/MapMaker/AreaDefiner/area_maker.gd
@@ -4,13 +4,20 @@ extends PanelContainer
 @onready var bg = %BgPreview
 
 var areaname: String
+var bg_img: String
 var links: Array
 var tags: Array
 var items: Array
 
 @onready var area_info = {
 	"name": areaname,
+	"bg": bg_img,
 	"links": [links],
 	"tags": [tags],
 	"items": [items],
 	}
+
+func _on_area_name_input_text_changed() -> void:
+	areaname = %AreaNameInput.text
+	print(areaname)
+	pass # Replace with function body.
