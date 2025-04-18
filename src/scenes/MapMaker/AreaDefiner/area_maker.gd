69c19c447bb55fe2e0e1d783dc1f4a8daf44beb4
Wayne <crazyfredsteve@gmail.com>
2025-04-18 14:12:14 -0400

    Started implementing a function/signalthat updates each area independently of one another
    Added a signal to the AreaDefiner base script that emits whenever one of it's details is changed, this just passes all the info regarding the current area back to the map maker to ensure it has all the info it needs.

--- src/scenes/MapMaker/AreaDefiner/area_maker.gd
+++ src/scenes/MapMaker/AreaDefiner/area_maker.gd
@@ -1,10 +1,11 @@
 extends PanelContainer
+signal update_map(area_info)
 
 @onready var changebg = %ChangeBgImage
 @onready var bg = %BgPreview
 
 var areaname: String
-var bg_img
+var bg_img: Resource
 var links: Array 
 var tags: Array
 var items: Array
@@ -12,16 +13,19 @@ var items: Array
 @onready var area_info = {
 	"name": areaname,
 	"bg": bg_img,
-	"links": [links],
-	"tags": [tags],
-	"items": [items],
+	"links": links,
+	"tags": tags,
+	"items": items,
 	}
 
 func _on_area_name_input_text_changed() -> void:
 	areaname = %AreaNameInput.text
 	print(areaname)
-	pass # Replace with function body.
+	send_map_update()
 
 func _on_change_bg_image_background_passer(bg: Variant) -> void:
-	bg_img = bg
-	pass # Replace with function body.
+	bg_img = bg 
+	send_map_update()
+
+func send_map_update():
+	emit_signal("update_map", area_info)
--- src/scenes/MapMaker/map_maker.gd
+++ src/scenes/MapMaker/map_maker.gd
@@ -5,9 +5,12 @@ extends Control
 @onready var map: Array = []
 
 func _on_new_area_area_passer(area: Variant) -> void:
-	map.append(area)
+	map.append(area.area_info)
 	pass # Replace with function body.
-
 
 func _on_print_map_pressed() -> void:
+	print(map)
 	pass # Replace with function body.
+
+func update_map():
+	pass
