092bca10716ba30dc03c426c822ebfe2d91eedfb
Wayne <crazyfredsteve@gmail.com>
2025-04-18 23:01:48 -0400

    Removed roadmap button from cast maker and put it on the main menu
    Used the exact same code as well.
    Linked it up to the base node like the other two buttons.

--- src/scenes/MainMenu/menu_directories.gd
+++ src/scenes/MainMenu/menu_directories.gd
@@ -1,5 +1,7 @@
 extends PanelContainer
 
+var target_url = "https://trello.com/b/cXJdfr6D/killing-fever-royale-roadmap"
+
 func _on_cast_builder_pressed() -> void:
 	get_tree().change_scene_to_file("uid://c62mvng83jemu")
 	pass # Replace with function body.
@@ -7,3 +9,13 @@ func _on_cast_builder_pressed() -> void:
 func _on_map_builder_pressed() -> void:
 	get_tree().change_scene_to_file("uid://d2yjq2dua1vf8")
 	pass # Replace with function body.
+
+func _on_road_map_pressed() -> void:
+	open_url_in_browser(target_url)
+
+func open_url_in_browser(url: String) -> void:
+	
+	if url.begins_with("http://") or url.begins_with("https://"):
+		OS.shell_open(url)
+	else:
+		push_error("Invalid URL: Must start with http:// or https://")
