f6cdb13f58bab465a6260ea460bdbae20a9e79e1
Wayne <crazyfredsteve@gmail.com>
2025-04-18 22:56:21 -0400

    Buttons now change scenes

---
+++ src/scenes/MainMenu/menu_directories.gd
@@ -0,0 +1,9 @@
+extends PanelContainer
+
+func _on_cast_builder_pressed() -> void:
+	get_tree().change_scene_to_file("uid://c62mvng83jemu")
+	pass # Replace with function body.
+
+func _on_map_builder_pressed() -> void:
+	get_tree().change_scene_to_file("uid://d2yjq2dua1vf8")
+	pass # Replace with function body.
